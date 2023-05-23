2150d121108e3b399309de406b6f15d50178e5a1
hungnc89 <72601287+hungnc89@users.noreply.github.com>
2023-05-23 12:45:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,61 @@
 #!/bin/bash
 
 # Update system packages
-sudo apt-get update
+apt-get update
 
 # Install build essentials
-sudo apt-get install -y build-essential
+apt-get install -y build-essential
 
 # Install Python 3.8 and pip
-sudo apt-get install -y python3.8 python3-pip
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+apt-get install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3.8 python3-pip
 
 # Install NodeJS 18.X and npm
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
+npm install -g npm@latest
 
 # Clone the repository
 git clone https://github.com/miroslavpejic85/mirotalksfu.git
 
-# Navigate to the mirotalksfu directory
+# Go to the mirotalksfu directory
 cd mirotalksfu
 
-# Copy app/src/config.template.js to app/src/config.js
+# Copy the configuration file
 cp app/src/config.template.js app/src/config.js
 
-# Update the announcedIp in app/src/config.js
-sed -i "s/'Server Public IPv4'/'$(curl -s ifconfig.me)'/" app/src/config.js
+# Get the server's public IP address
+server_ip=$(curl -s ifconfig.me)
+
+# Replace 'getLocalIp()' with the server's IP address in app/src/config.js
+sed -i "s/getLocalIp()/'$server_ip'/g" app/src/config.js
 
 # Install dependencies
 npm install
 
 # Install Nginx
-sudo apt-get install -y nginx
+apt-get install -y nginx
 
-# Configure Nginx
-sudo tee /etc/nginx/sites-available/mirotalk <<EOF
+# Install Certbot (SSL certificates)
+apt-get install -y snapd
+snap install core; snap refresh core
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+# Configure Nginx sites
+cat > /etc/nginx/sites-available/mirotalk << EOF
 server {
+    if (\$host = sfu.mirotalk.haiphong.online) {
+        return 301 https://\$host\$request_uri;
+    }
+
     listen 80;
+    listen [::]:80;
+
     server_name sfu.mirotalk.haiphong.online;
 
     location / {
@@ -49,6 +70,8 @@ server {
 
 server {
     listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+
     server_name sfu.mirotalk.haiphong.online;
 
     ssl_certificate /etc/letsencrypt/live/sfu.mirotalk.haiphong.online/fullchain.pem;
@@ -65,21 +88,33 @@ server {
 }
 EOF
 
-# Enable the site
-sudo ln -s /etc/nginx/sites-available/mirotalk /etc/nginx/sites-enabled/
+# Create a symbolic link for the Nginx site
+ln -s /etc/nginx/sites-available/mirotalk /etc/nginx/sites-enabled/mirotalk
 
 # Test Nginx configuration
-sudo nginx -t
+nginx -t
 
 # Restart Nginx
-sudo systemctl restart nginx
+systemctl restart nginx
 
-# Obtain SSL certificate with Certbot
-sudo apt-get install -y certbot python3-certbot-nginx
-sudo certbot --nginx -d sfu.mirotalk.haiphong.online
+# Allow ports for Mirotalk and SSH
+ufw allow 3010
+ufw allow 22
 
-# Auto renew SSL certificate
-sudo certbot renew --dry-run
+# Enable UFW firewall
+ufw enable
 
-# Check the installed certificates
-sudo certbot certificates
+# Install PM2
+npm install pm2 -g
+
+# Start the application with PM2
+pm2 start server.js
+
+# Save the PM2 process list and enable auto-start on boot
+pm2 save
+pm2 startup
+
+# Renew SSL certificate
+certbot renew --dry-run
+
+echo "Mirotalk SFU setup completed successfully."
